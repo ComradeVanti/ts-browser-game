a7ba743bfc41b74293c813de27c44d1e369eead2
Ramon Brullo <kingvanti650@gmail.com>
2023-12-21 12:42:06 +0100

    deps: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.ts",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: "public", to: "." }],
+        }),
+    ],
+    resolve: {
+        extensions: [".ts"],
+    },
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+};
